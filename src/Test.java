52b30693fb35a8f96d0f1aeb13f9d49d373f5b0f
lukasz <robson27-r@hotmail.com>
2019-02-13 13:37:56 +0100

    w trakcie

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Test {
+    public static void main(String[] args) {
+
+        Person person = new Person("Adam", "Podolski", 25,"23109030");
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Podaj informacje o osobie");
+
+
+    }
+}
